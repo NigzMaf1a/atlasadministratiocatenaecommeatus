255380c0c05b0eeaeec2e3cdbe35d236c01e18b1
Nigzmaf1a <Nigelkhasiani@gmail.com>
2026-08-23 13:16:33 +0300

    feat:add initial files

---
+++ src/main/java/com/nigel/atlas_administratio_catenae_commeatus/AtlasAdministratioCatenaeCommeatusApplication.java
@@ -0,0 +1,13 @@
+package com.nigel.atlas_administratio_catenae_commeatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasAdministratioCatenaeCommeatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasAdministratioCatenaeCommeatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nigel/atlas_administratio_catenae_commeatus/AtlasAdministratioCatenaeCommeatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nigel.atlas_administratio_catenae_commeatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasAdministratioCatenaeCommeatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
