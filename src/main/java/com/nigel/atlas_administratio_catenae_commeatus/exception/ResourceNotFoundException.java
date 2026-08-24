4d0dfb598a354ba98e4bae4fc22fb9bef86de90e
Nigzmaf1a <Nigelkhasiani@gmail.com>
2026-08-24 18:32:35 +0300

    feat:added a resource not found exception

---
+++ src/main/java/com/nigel/atlas_administratio_catenae_commeatus/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.nigel.atlas_administratio_catenae_commeatus.exception;
+
+public class ResourceNotFoundException
+        extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
